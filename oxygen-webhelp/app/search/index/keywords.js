967ef17faa22a4867b25842688da6881e024afb8
rayoptiva <107554883+rayoptiva@users.noreply.github.com>
2022-09-14 18:01:08 +0100

    first commit

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,24 @@
+define(function() {var keywords=[{w:"Accounts",p:["p0"]},{w:"Cases",p:["p1"]},{w:"Creating",p:["p2","p8"]},{w:"a",p:["p2","p12"]},{w:"party",p:["p2","p9","p12"]},{w:"Create",p:["p3","p4"]},{w:"Account",p:["p3","p13"]},{w:"Subscriptions",p:["p4","p16"]},{w:"Context",p:["p5"]},{w:"Sensitive",p:["p5"]},{w:"Help",p:["p5"]},{w:"Customer",p:["p6"]},{w:"Home",p:["p7"]},{w:"customers",p:["p8","p11"]},{w:"Dashboard",p:["p10"]},{w:"Editing",p:["p11","p12"]},{w:"Edit",p:["p13","p14"]},{w:"Subscription",p:["p14"]},{w:"Settings",p:["p15"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1];
+ph["p2"]=[2, 3, 4];
+ph["p3"]=[5, 6];
+ph["p4"]=[5, 7];
+ph["p5"]=[8, 9, 10];
+ph["p6"]=[11];
+ph["p7"]=[12];
+ph["p8"]=[2, 13];
+ph["p9"]=[4];
+ph["p10"]=[14];
+ph["p12"]=[15, 3, 4];
+ph["p11"]=[15, 13];
+ph["p14"]=[16, 17];
+ph["p13"]=[16, 6];
+ph["p16"]=[7];
+ph["p15"]=[18];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {7:-1,0:-1,11:6,14:16,4:16,15:-1,2:9,16:-1,1:-1,13:0,12:9,6:-1,10:7,9:-1,8:6,3:0};
+});
