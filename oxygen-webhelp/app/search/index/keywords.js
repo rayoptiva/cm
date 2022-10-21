72e3ba9551fe0102f262fd9919841f2fd74a2851
rayoptiva <107554883+rayoptiva@users.noreply.github.com>
2022-10-21 15:51:11 +0100

    update21.10

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"Accounts",p:["p0"]},{w:"Cases",p:["p1"]},{w:"Creating",p:["p2","p4","p8","p12"]},{w:"a",p:["p2","p7","p16","p19"]},{w:"Party",p:["p2","p13","p16"]},{w:"Create",p:["p3"]},{w:"Account",p:["p3","p17"]},{w:"Subscriptions",p:["p4","p24"]},{w:"Context",p:["p5"]},{w:"Sensitive",p:["p5"]},{w:"Help",p:["p5"]},{w:"Closed",p:["p6","p8","p9"]},{w:"User",p:["p6","p8","p9"]},{w:"Groups",p:["p6","p8","p9"]},{w:"Adding",p:["p7","p19"]},{w:"New",p:["p7"]},{w:"Member",p:["p7"]},{w:"Editing",p:["p9","p15","p16"]},{w:"Customer",p:["p10"]},{w:"Home",p:["p11"]},{w:"Customers",p:["p12","p15"]},{w:"Dashboard",p:["p14"]},{w:"Edit",p:["p17","p18"]},{w:"Subscription",p:["p18"]},{w:"Personal",p:["p19","p20"]},{w:"List",p:["p19","p20"]},{w:"Plan",p:["p19"]},{w:"Plans",p:["p20"]},{w:"Settings",p:["p21"]},{w:"Usage",p:["p22","p23"]},{w:"Details",p:["p22"]},{w:"and",p:["p23"]},{w:"Balance",p:["p23"]}];
+define(function() {var keywords=[{w:"Accounts",p:["p0"]},{w:"Cases",p:["p1"]},{w:"Creating",p:["p2","p4","p8","p12"]},{w:"a",p:["p2","p7","p16","p21"]},{w:"Party",p:["p2","p13","p16"]},{w:"Create",p:["p3"]},{w:"Account",p:["p3","p17"]},{w:"Subscriptions",p:["p4","p27"]},{w:"Context",p:["p5"]},{w:"Sensitive",p:["p5"]},{w:"Help",p:["p5"]},{w:"Closed",p:["p6","p8","p9"]},{w:"User",p:["p6","p8","p9"]},{w:"Groups",p:["p6","p8","p9"]},{w:"Adding",p:["p7","p21"]},{w:"New",p:["p7"]},{w:"Member",p:["p7"]},{w:"Editing",p:["p9","p15","p16"]},{w:"Customer",p:["p10"]},{w:"Home",p:["p11"]},{w:"Customers",p:["p12","p15"]},{w:"Dashboard",p:["p14"]},{w:"Edit",p:["p17","p18"]},{w:"Subscription",p:["p18"]},{w:"Flex",p:["p19"]},{w:"Bundles",p:["p19"]},{w:"Pin",p:["p20"]},{w:"Management",p:["p20"]},{w:"Personal",p:["p21","p22"]},{w:"List",p:["p21","p22"]},{w:"Plan",p:["p21"]},{w:"Plans",p:["p22"]},{w:"Recharge",p:["p23"]},{w:"History",p:["p23"]},{w:"Settings",p:["p24"]},{w:"Usage",p:["p25","p26"]},{w:"Details",p:["p25"]},{w:"and",p:["p26"]},{w:"Balance",p:["p26"]}];
 var ph={};
 ph["p0"]=[0];
 ph["p1"]=[1];
@@ -19,12 +19,15 @@ ph["p16"]=[17, 3, 4];
 ph["p15"]=[17, 20];
 ph["p18"]=[22, 23];
 ph["p17"]=[22, 6];
-ph["p19"]=[14, 3, 24, 25, 26];
-ph["p21"]=[28];
-ph["p20"]=[24, 25, 27];
-ph["p23"]=[29, 31, 32];
-ph["p22"]=[29, 30];
-ph["p24"]=[7];
+ph["p19"]=[24, 25];
+ph["p21"]=[14, 3, 28, 29, 30];
+ph["p20"]=[26, 27];
+ph["p23"]=[32, 33];
+ph["p22"]=[28, 29, 31];
+ph["p25"]=[35, 36];
+ph["p24"]=[34];
+ph["p27"]=[7];
+ph["p26"]=[35, 37, 38];
      return {
          keywords: keywords,
          ph: ph
